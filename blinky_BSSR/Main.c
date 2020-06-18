33d49c5a090211e232066953a11c3b7de3b91060
Utkarsh Sethi <utkarsh.sethi7@gmail.com>
2020-06-18 15:05:07 +0530

    Update

---
+++ blinky_BSSR/Main.c
@@ -0,0 +1,18 @@
+//BSRR
+
+/*
+	GPIOx_BSRR	
+ 
+*	LEDs
+	Green		- PD12
+	Orange 	- PD13
+	RED			-	PD14
+	BLUE		- PD15
+	PORT D - GPIODEN
+	AHB1
+	RCC -> AHB1ENR
+	GPIOx_MODER -	Mode
+	GPIOx_ODR 	-	O/p [ 0-15]
+	
+	
+	
