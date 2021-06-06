05cb8fad4281e3e3241c85cd08aab1f115d2bc41
utkarsh.sethi7 <utkarsh.sethi7@gmail.com>
2021-06-06 18:21:31 +0530

    --

--- UART_TX_RX/main_old.c
+++ UART_TX_RX/main_old.c
@@ -37,11 +37,36 @@ USART_BRR (Baud	Rate Register)
 /******************************************************************************/
 
 #include "stm32f4xx.h"
-
+#include <stdint.h>
 #define	toDigit(c) (c-'0')    //convert	number received	in char	to int
 			      //  C/C++	standard work on char encodings	where '0' - '9'	are always together
 			      //  '9' -	'0' will remove	starting  ascii	of '0' from '9'	resulting in the actual	number i.e. 9
-			      //https://stackoverflow.com/questions/439573/how-to-convert-a-single-char-into-an-int
+
+/*
+APB1 : USART3 [RCC -> APB1 | Bit18 (USART3_EN) ]
+
+USART3 >
+    > TX  PD-8
+    > RX  PD-9
+	Use port D instead of B	as we already enable clock to GPIO D for LEDs
+
+USART_SR
+
+USART_CR1
+    > RE - receiver enable > bit 2
+    > TE - transmitter enable >	bit 3
+    > M	> Bit 12 (word length)
+    > UE > Bit 13 (uart	enable)
+
+AF register
+GPIOx_AFRH (8..15) > AF7 (USART1..3)
+
+USART_BRR (Baud	Rate Register)
+    >
+
+*/
+    uint32_t Pclk     =	16000000;
+    uint32_t BaudRate =	115200;
 
 /******************************************************************************/
 /*
@@ -60,6 +85,27 @@ void sudo_delay(int delay)
 	    __asm("nop");
 	}
     }
+}
+
+
+/******************************************************************************/
+/*
+    BAUD = Fck/(16*USARTDIV)
+    i.e. USARTDIV = PCLK1/(Baud*16)
+      @Baud=115200
+      @PCLK1= 16 MHZ = 16000000	Hz
+	USARTDIV = 8.680
+	  i.e.	BRR_mantissa  =	8 = 0x8
+		BRR_frac      =	0.680*16 = 10.88  = 11 =0xB
+	Hence BRR = 0x8B
+*/
+/******************************************************************************/
+//BRR =	(Pclk +	(BaudRate / 2))	/ BaudRate
+
+static uint16_t	calc_BRR(uint32_t Pclk,	uint32_t BaudRate)
+{
+  uint16_t brr = (Pclk + (BaudRate / 2)) / BaudRate;
+  return  brr;
 }
 
 /******************************************************************************/
@@ -93,7 +139,9 @@ void init() {
     GPIOD->MODER  |= GPIO_MODER_MODER8_1	  // 0b10 > AF Mode
 		  | GPIO_MODER_MODER9_1;	  //set	PD8,9 to AF
 
-    GPIOD->AFR[1] |= (0b111 << 4 * 0) |	(0b111 << 4 * 1);   //	0111(9)	0111(8)	>  0x77	put PD8,9 as AF7
+
+    //	0111(9)	0111(8)	>  0x77	put PD8,9 as AF7
+    GPIOD->AFR[1] |= (0b111 << 4 * 0) |	(0b111 << 4 * 1);    //blocks of 4bits.	So firs	t4 bits	= 4*0 and then 4*1 etc....
 
 /*
     BAUD = Fck/(16*USARTDIV)
@@ -104,7 +152,10 @@ void init() {
 		BRR_frac      =	0.680*16 = 10.88  = 11 =0xB
 	Hence BRR = 0x8B
     */
-    USART3->BRR	|= 0x8B;    //set baurdate	= 115200
+    uint32_t Pclk     =	16000000;
+    uint32_t BaudRate =	115200;
+
+    USART3->BRR	|= calc_BRR(Pclk, BaudRate);	//set baurdate	= 115200
 
     USART3->CR1	|= (1 << 3);	//Bit 3	TE: Transmitter	enable
     USART3->CR1	|= (1 << 2);	//Bit 2	RE: Receiver enable
@@ -129,16 +180,27 @@ void init() {
 */
 /******************************************************************************/
 
-void usart3_write(int ch)
+//void usart3_write(int	ch)
+//{
+//    //check	Tx buffer is empty
+//    while(!(USART3->SR & (1 << 7))) {
+//    };			//Bit	7 TXE: Transmit	data register empty
+//    USART3->DR = (ch & 0xFF);	/* '&0xFF' makes	sure value	sent is	8bits (my guess). Might	be better ways to implement
+//				     *	'&0x7F'	might be used when parity is  enables and data is 7 bits. (agin	my guess)
+//				     */
+//}
+
+//write	single char to USART
+int usart3_putc(int ch)
 {
     //check	Tx buffer is empty
-    while(!(USART3->SR & (1 << 7))) { };  //Bit	7 TXE: Transmit	data register empty
-	USART3->DR = (ch & 0xFF);     /* '&0xFF' makes	sure value	sent is	8bits (my guess). Might	be better ways to implement
+    while(!(USART3->SR & (1 << 7))) { //Wait while Tx Buffer is	not empty
+    };			      //Bit	7 TXE: Transmit	data register empty
+    USART3->DR = (ch & 0xFF); /*	'&0xFF'	makes	sure value	sent is	8bits (my guess). Might	be better ways to implement
 				     *	'&0x7F'	might be used when parity is  enables and data is 7 bits. (agin	my guess)
 				     */
-
+    return ch;
 }
-
 /******************************************************************************/
 /*
       usart_read()
@@ -148,13 +210,22 @@ void usart3_write(int ch)
 */
 /******************************************************************************/
 
-char usart3_read()
+//char usart3_read()
+//{
+
+//    while(!(USART3->SR & (1 << 5))) {
+//    };    //Bit	5 RXNE:	Read data	register not empty; RXNE = 1
+
+//    return USART3->DR;
+//}
+
+//read single char from	USART
+int usart3_getc()
 {
+    while(!(USART3->SR & (1 << 5))) {	//Wait while Rx	Buffer is empty	or wait	till it	is not empty
+    };	  //Bit	5 RXNE:	Read data	register not empty; RXNE = 1
 
-    while(!(USART3->SR & (1<<5)))	 { };  //Bit	5 RXNE:	Read data	register not empty; RXNE = 1
-
-      return USART3->DR;
-
+    return USART3->DR;
 }
 
 /******************************************************************************/
@@ -196,13 +267,13 @@ int main(void)
 	if(GPIOA->IDR &	GPIO_IDR_ID0) {	   // if	PA0	is high
 					   //Read data	from Rx
 
-	    number = toDigit(usart3_read());
+	    number = toDigit(usart3_getc());
 	    led(number);
 	}
 	else {
 	    //	Send one char at a time.
 	    for(int i =	0; i <=	size_my_string;	i++) {
-		usart3_write(my_string[i]);
+		usart3_putc(my_string[i]);
 		sudo_delay(10);
 	    }
 	    //sudo_delay(100);
